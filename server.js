208869afa3adad055b8ab178fd02afe51035d760
Roneel Valambhia <roneelmv@gmail.com>
2022-05-08 04:38:55 +0530

    feat: complete homomorphic offset calculation
    automate bounds calculation from the geoJSON file
    
    fix CORS

--- server.js
+++ server.js
@@ -1,24 +1,44 @@
-const express = require('express')
-const JSONbig = require('json-bigint')
-const { findGeoFences } = require('./geofence');
+const express = require("express");
+const JSONbig = require("json-bigint")({
+  alwaysParseAsBig: true,
+  useNativeBigInt: true,
+});
+const { calcOffsets } = require("./calcOffsets");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.post("/location", async (req, res) => {
-	// const { x, y, n, g } = req.body;
-	console.log(JSONbig.parse(req.body.reqdata));
-	const geofences = await findGeoFences(x, y, n, g);
-	console.log(JSONbig.parse(JSONbig.stringify(geofences)));
-	res.json({ offsetStr: JSONbig.stringify(geofences) });
+app.get("/", (req, res) => {
+  res.send(
+    "Hello, anon! It seems you have reached this webpage, though I'm afraid you won't find it very useful."
+  );
+});
+
+app.post("/locate", async (req, res) => {
+  try {
+    const { payload } = req.body;
+
+    const { lat, lng, n, g } = JSONbig.parse(payload);
+
+    const geofences = await calcOffsets(lat, lng, n, g);
+
+    res.json({ offsetStr: JSONbig.stringify(geofences) });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 app.listen(PORT, () => {
-	console.log(`Server is listening on port ${PORT}...`);
+  console.log(`Server is listening on port ${PORT}...`);
 });
